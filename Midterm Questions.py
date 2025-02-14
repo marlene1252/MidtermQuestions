7bbfad5bc9b833dfe1d712b0093e2351e11342ca
marlenepfiester <marlenepfiester@gmail.com>
2025-02-14 11:39:32 +0100

    midterm questions

---
+++ Midterm Questions.py
@@ -0,0 +1,29 @@
+type(2+3) #Use IDLE
+type(6/2) #this is a float, add another / to make it into int
+
+#indicating the results
+print(5 or 6)
+print(5 and 6 and 7)
+print([1,2,3].append("John")) # gives none
+print("bubu"*2)
+
+# assume that the operations are executed in order. what will each print display
+a = 2
+b = 3
+c = "abc"
+
+print(a)
+print(b)
+print(c)
+print(a, b, c)
+print(a, b, c, sep=",")
+a += 2
+a == 5
+print(a)
+print(c*(a - b))
+d = c.find("b")
+print(d)
+print(d and b)
+print( d == True)
+e = str(a) + str(b) + str(c) + str(d)
+
---
+++ function_multiple_of_6.py
@@ -0,0 +1,2 @@
+# write a function that forces the user to enter a multiple of 6 number
+# use try, except to catch valid inputs
